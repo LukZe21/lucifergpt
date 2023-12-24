33b7ebe98b7e44d024ce57af9b6c80ee67561b44
LukZe21 <154433419+LukZe21@users.noreply.github.com>
2023-12-24 15:27:33 +0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request
+from main import get_answer
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+user_requests = []
+lucifer_response = []
+@app.route('/chat', methods=['POST'])
+def process_text():
+    user_msg = request.form.get('user_message')
+
+    lucifer_gpt = get_answer(user_msg)
+
+    user_requests.append(user_msg)
+    lucifer_response.append(lucifer_gpt)
+
+    return render_template('index.html', user=user_requests, gpt=lucifer_response)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
