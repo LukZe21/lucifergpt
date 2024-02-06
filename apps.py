7f1b0ef689c04145e2330f1fbac311206c8e3e3b
LuKee21 <gam3rlinfo@gmail.com>
2024-02-06 16:34:00 +0400

    'update'

--- apps.py
+++ apps.py
@@ -4,22 +4,36 @@ from main import get_answer
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return render_template('index.html')
-
 user_requests = []
 lucifer_response = []
-@app.route('/chat', methods=['GET', 'POST'])
-def process_text():
-    user_msg = request.form.get('user_message')
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.form.get('user_message'):
+        user_msg = request.form.get('user_message')
 
-    lucifer_gpt = get_answer(user_msg)
+        lucifer_gpt = get_answer(user_msg)
 
-    user_requests.append(user_msg)
-    lucifer_response.append(lucifer_gpt)
+        user_requests.append(user_msg)
+        lucifer_response.append(lucifer_gpt)
 
-    return render_template('index.html', user=user_requests, gpt=lucifer_response)
+        return render_template('index.html', user=user_requests, gpt=lucifer_response)
+    
+    return render_template('index.html')
+
+
+
+# user_requests = []
+# lucifer_response = []
+# @app.route('/chat', methods=['GET', 'POST'])
+# def process_text():
+#     user_msg = request.form.get('user_message')
+
+#     lucifer_gpt = get_answer(user_msg)
+
+#     user_requests.append(user_msg)
+#     lucifer_response.append(lucifer_gpt)
+
+#     return render_template('index.html', user=user_requests, gpt=lucifer_response)
 
 
 if __name__ == '__main__':
